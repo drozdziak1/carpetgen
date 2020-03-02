6126a974430cd86821e85224c87854fa9e7423b4
Stan Drozd <drozdziak1@gmail.com>
2020-03-02 15:52:55 +0100

    Start learning Vue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+mod routes;
+
+// src/main.rs
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use dotenv::dotenv;
+use listenfd::ListenFd;
+
+use std::env;
+
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(||
+                                     App::new()
+                                     .configure(routes::init_routes)
+                                    );
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Host not set");
+            let port = env::var("PORT").expect("Port not set");
+            server.bind(format!("{}:{}", host, port))?},
+    };
+
+    info!("Starting server");
+    server.run().await
 }
---
+++ src/routes.rs
@@ -0,0 +1,44 @@
+use actix_web::{get, web, HttpResponse, Responder};
+use actix_files::NamedFile;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+use std::path::PathBuf;
+
+#[derive(Clone, Default, Deserialize, Serialize)]
+struct Carpet {
+    pub id: i32,
+    pub name: String,
+    pub x: f64,
+    pub y: f64,
+    pub w: f64, // longitude
+    pub h: f64, // latitude
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let p: PathBuf = "static/index.html".parse().unwrap();
+
+    NamedFile::open(p)
+}
+
+#[get("/carpets")]
+async fn carpets() -> impl Responder {
+    let carpets: Vec<Carpet> = vec![
+        Carpet {
+            id: 1,
+            name: "My carpet".to_owned(),
+            x: 5.0,
+            y: 5.0,
+            w: 5.01,
+            h: 5.01,
+        },
+        Default::default(),
+    ];
+    HttpResponse::Ok().json(carpets)
+}
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(index);
+    cfg.service(carpets);
+}
