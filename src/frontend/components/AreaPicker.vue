7696cce4c945c631d4e2a3941bb55059e01e8390
Stan Drozd <drozdziak1@gmail.com>
2020-04-22 11:15:47 +0200

    Style AreaPicker and PickerResults to float left

--- src/frontend/components/AreaPicker.vue
+++ src/frontend/components/AreaPicker.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="area-picker" ref="areaPickerEl">
-    <p>Choose carpet shape and orientation</p>
+    <h2>Carpet shape and orientation</h2>
     <select id="carpet-shape-dropdown" v-model="aspectRatio">
       <option value="1:1">1:1</option>
       <option value="2:1">2:1</option>
@@ -10,6 +10,13 @@
     <Map :aspect-ratio="aspectRatio" :is-vertical="isVertical" :unit="200" />
   </div>
 </template>
+
+<style lang="scss">
+#area-picker {
+  float: left;
+  width: 50%;
+}
+</style>
 
 <script>
 import Map from "./Map.vue";
--- src/frontend/components/PickerResults.vue
+++ src/frontend/components/PickerResults.vue
@@ -1,6 +1,14 @@
 <template>
+<div id="picker-results">
   <h2>Results</h2>
+</div>
 </template>
+
+<style lang=scss>
+#picker-results {
+float: left;
+}
+</style>
 
 <script>
 export default {};
