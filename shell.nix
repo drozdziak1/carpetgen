1321047fc821391494454e50755203f7b1efbcab
Stan Drozd <drozdziak1@gmail.com>
2020-05-06 15:54:18 +0200

    Add Nix support:
    This commit adds Nix support via Niv, Lorri and Naersk.
    
    .envrc:
    * Add for automating lorri via direnv
    
    carpetgen.nix:
    * Add a naersk rust package definition
    
    common_deps.nix:
    * Add common nixpkgs dependencies
    
    nix/rust.nix:
    * Add rust sources with nixpkgs-mozilla
    
    nix/sources.[json | nix]:
    * niv-generated files
    
    shell.nix:
    * script for lorri shell

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  sources = import ./nix/sources.nix;
+  rust = import ./nix/rust.nix { inherit sources;  };
+  pkgs = import sources.nixpkgs {};
+  deps = import ./common_deps.nix;
+in
+pkgs.mkShell {
+  buildInputs = [
+    rust
+  ] ++ deps {inherit pkgs;};
+}
